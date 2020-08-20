3f6cc48eabb152c857979af73e1ca785ce87bc86
Sam Arossi Maosa <smaosa@strathmore.edu>
2020-08-20 08:20:29 +0300

    Moved savadmin package outside the project

--- savannabits/savadmin/resources/views/api-routes.blade.php
+++
@@ -1,9 +0,0 @@
-/* Auto-generated {{$modelRouteName}} api routes */
-Route::group(['prefix' => $apiPrefix,'as' => 'api.', 'namespace' => $apiNamespace], function() {
-    Route::group(['middleware' => ["auth:sanctum","verified"]], function() {
-        Route::group(['prefix' => "{{$modelRouteName}}", 'as' => '{{$modelRouteName}}.'],function() {
-            Route::get("dt", "{{$controllerClassName}}@dt")->name('dt');
-        });
-        Route::apiResource('{{$modelRouteName}}',"{{$controllerClassName}}")->parameters(["{{$modelRouteName}}" => "{{$modelVariableName}}"]);
-    });
-});
